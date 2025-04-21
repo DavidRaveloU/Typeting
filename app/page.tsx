b9fdebbdd174c30dec877ed887c4acfc5442c68b
DavidRaveloU <dravelo@unicesar.edu.co>
2025-04-21 16:37:49 -0500

    feat: wrap Home component in TypingConfigProvider for context management

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import TypingArea from "@/components/TypingArea";
+import { TypingConfigProvider } from "@/context/TypingConfigContext";
 
 export default function Home() {
   return (
-    <>
+    <TypingConfigProvider>
       <Navbar />
       <Header />
       <TypingArea />
-    </>
+    </TypingConfigProvider>
   );
 }
